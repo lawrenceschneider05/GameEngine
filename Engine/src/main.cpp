423e1f44085b7f80a5075d395e336035fc52b252
Lawrence Schneider <Lawrence_Schneider@outlook.com>
2022-12-28 18:03:05 -0800

    glfw window in main

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -2,8 +2,56 @@
 #include <glad/glad.h>
 #include <vendor/include/GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
 int main(int argc, char** argv)
 {
 	std::cout << "Hello Game Engine!\n";
-	std::cin.get();
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 32);
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 640, "Oak Game Engine", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	glfwSwapInterval(1);
+
+
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize OpenGL.";
+	}
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+	glfwMaximizeWindow(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, true);
+		}
+
+		glfwSwapBuffers(window);
+	}
 }
